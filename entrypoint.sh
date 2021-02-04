9737090fd7ef485bdbd75f1895c230b5504df1f1
Natan Camargos <natanscamargos@gmail.com>
2021-02-04 00:16:56 -0300

    Adding fallbackfee

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 cd regtest
 echo -e "\nStarting both bitcoin nodes...\n"
-bitcoind -datadir=/regtest/node1
-bitcoind -datadir=/regtest/node2
+bitcoind -regtest -deprecatedrpc=accounts -datadir=/regtest/node1
+bitcoind -regtest -deprecatedrpc=accounts -datadir=/regtest/node2
 
 function node1(){
-    bitcoin-cli -regtest -datadir=/regtest/node1 -rpcport=1112 $@ > /dev/null 2>&1
+    bitcoin-cli -regtest -datadir=/regtest/node1 -rpcuser=root -rpcpassword=root -rpcport=1112 $@
 }
 function node2() {
-    bitcoin-cli -regtest -datadir=/regtest/node2 -rpcport=2223 $@ > /dev/null 2>&1
+    bitcoin-cli -regtest -datadir=/regtest/node2 -rpcuser=root -rpcpassword=root -rpcport=1114 $@
 }
 
 export -f node1;
@@ -16,7 +16,8 @@ export -f node2;
 
 sleep 3
 
-node1 -rpcuser=root -rpcpassword=root generatetoaddress 1 bcrt1qu34xuknwltm7278jkwxetfqfl89q73h2eeyfzv
+node1 createwallet "" > /dev/null 2>&1 || true 
+node1 generatetoaddress 1 bcrt1qu34xuknwltm7278jkwxetfqfl89q73h2eeyfzv  > /dev/null 2>&1 
 
 echo -e "\Bitcoind regtest started!\n"
 
